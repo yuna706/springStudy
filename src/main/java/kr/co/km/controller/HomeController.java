dde7a4eef78455e825c2c0ac3aa2f7eda7db8621
yuna706 <riyu6027@naver.com>
2022-10-14 20:48:06 +0900

    feat: add default uri login logic
    Signed-off-by: yuna706 <riyu6027@naver.com>

--- src/main/java/kr/co/km/controller/HomeController.java
+++ src/main/java/kr/co/km/controller/HomeController.java
@@ -1,32 +1,19 @@
 package kr.co.km.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import lombok.ToString;
+import javax.servlet.http.HttpSession;
 
 @Controller
-@ToString
 public class HomeController {
 
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate);
-
-		return "home";
-	}
+    @GetMapping("/")
+    public String home(HttpSession session) {
+        if (session == null) {
+            return "redirect:/login";
+        } else {
+            return "redirect:/todoList";
+        }
+    }
 }
